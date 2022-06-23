0f45ca5d0592a5fde6b64f654eb1c6d97beba1f0
anbv731 <anbv731@yandex.ru>
2022-06-23 22:13:46 +0400

    bottom nav complete

--- app/src/main/java/com/example/rickandmortyaston/presentation/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyaston/presentation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.rickandmortyaston.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rickandmortyaston.presentation.characters.CharactersFragment
 import com.example.rickandmortyaston.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,27 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentPlace, CharactersFragment(), null)
             .commit()
+setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.characters -> {
+                    Toast.makeText(this, "Characters selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.locations -> {
+                    Toast.makeText(this, "locations selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.episodes -> {
+                    Toast.makeText(this, "episodes selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> true
+            }
+        }
     }
 }
